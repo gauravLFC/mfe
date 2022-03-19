7c7b7e73babba0020673d47bcedc74ced53e2448
Gaurav Sharma <gauravsharma@Gauravs-MacBook-Air.local>
2022-03-19 11:58:28 +0530

    config changes for dashboard

---
+++ packages/dashboard/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJSON = require('../package.json');
+const commonConfig = require('./webpack.common');
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+        publicPath: '/dashboard/latest/'
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'dashboard',
+            exposes: {
+                './dashboardApp': './src/bootstrap'
+            },
+            filename: 'remoteEntry.js',
+            shared: packageJSON.dependencies
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, prodConfig);
